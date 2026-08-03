766be43995c394907d27b31b86d7e485f09af5b6
m1guel3agle <mian.ag123@gmail.com>
2026-08-03 14:53:18 -0500

    Tutorial 02 - MVC de libros con Express, TypeScript y EJS

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express';
+import { books } from '../data/books.js';
+import { Book } from '../models/Book.js';
 
 export class HomeController {
   static index(req: Request, res: Response): void {
@@ -20,5 +22,17 @@ export class HomeController {
     viewData["title"] = "Contact";
 
     res.render('home/contact', { viewData: viewData });
+  }
+
+  static Main_Point(req: Request, res: any) {
+    const viewData: any = {};
+    viewData["books"] = books;
+    res.render('home/books', viewData);
+  }
+
+  static show(req: any, res: any)
+  {
+    const book = Book.findById(books, parseInt(req.params.id));
+    res.render('home/show', { book: book })
   }
 }
